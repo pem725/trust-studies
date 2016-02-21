57463bccbd6dcc4e02a0b01dbc4c41e60ae2f25c
Eric Henry <eric.henry2172@gmail.com>
2016-02-21 14:51:19 -0500

    Fixed bug in db.php include

--- web/db.php
+++ web/db.php
@@ -1,7 +1,7 @@
 <?php
-    include 'config.php';
     
     function connect_to_db() {
+        include_once 'config.php';
         $DSN = 'mysql:dbname='. $DB . ';host=' . $DB_HOST;
         try {
             $conn = new PDO($DSN, $DB_USER, $DB_PASS);
